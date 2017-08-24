80600bbba11135ea38dd72bf3c6ba2dd833371d1
savara94 <stephon.savic@gmail.com>
2017-08-24 09:17:55 +0000

    everything seems fine, code could be refactored

--- app/main.c
+++ app/main.c
@@ -1,34 +1,52 @@
 #include <SDL2/SDL.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include "engine.h"
 #include "device.h"
 
-/*SDL_Window *o;
-SDL_Renderer *r;*/
-int32_t quit = 1;
+SDL_Window *o;
+SDL_Renderer *r;
+
+volatile int32_t quit = 0;
+
+void* handle_keyboard(void* param)
+{
+    param = NULL; /* Just to avoid warnings */
+    char q = 0;
+
+    while(1)
+    {
+        q = getchar();
+        if (q == 'q')
+	{
+	    quit = 1;
+	    break;
+	}
+    }
+
+    return param;
+}
 
 int32_t main()
 {
-    /*circle_t circle = 
+    pthread_t exit_thread;
+    circle_t circle =
     {
         {
             CENTER_X,
             CENTER_Y,
         },
       DEFAULT_RADIUS
-    };*/
-    
+    };
     nunchuk_t readings;
-    
     int32_t fd = open_device(NUNCHUK_DEVICE);
-    
     if (fd == RET_ERR)
     {
         return RET_ERR;
     }
 
-    /*SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO);
 
     o = SDL_CreateWindow("Nunchuk",
                             SDL_WINDOWPOS_UNDEFINED,
@@ -37,36 +55,42 @@ int32_t main()
                             HEIGHT_RESOLUTION,
                             SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 
-    r = SDL_CreateRenderer(o, -1, SDL_RENDERER_ACCELERATED);*/
+    r = SDL_CreateRenderer(o, -1, SDL_RENDERER_ACCELERATED);
 
-    while(quit)
+    pthread_create(&exit_thread, NULL, handle_keyboard, NULL);
+
+    while(!quit)
     {
         if (read_device(fd, &readings) == RET_ERR)
         {
             break;
         }
-        
+
         printf("%u %u %u %u\n", (unsigned int)readings.x_axis,
                (unsigned int)readings.y_axis,
                (unsigned int)readings.c_pressed,
                (unsigned int)readings.z_pressed);
-        
-        usleep(500000);
-        /*SDL_SetRenderDrawColor(r,0,0,255,255);
+
+	edit_circle(&circle, &readings);
+
+        usleep(3000);
+        SDL_SetRenderDrawColor(r,0,0,255,255);
         SDL_RenderClear(r);
         SDL_SetRenderDrawColor(r,0,0,0,255);
         draw_circle(r, &circle);
-        SDL_RenderPresent(r);*/
+        SDL_RenderPresent(r);
     }
-    
+
+    pthread_join(exit_thread, NULL);
+
     if (close_device(fd) == RET_ERR)
     {
         return RET_ERR;
     }
 
-    /*SDL_DestroyWindow(o);
+    SDL_DestroyWindow(o);
     SDL_DestroyRenderer(r);
-    SDL_Quit();*/
+    SDL_Quit();
 
     return 0;
 }
